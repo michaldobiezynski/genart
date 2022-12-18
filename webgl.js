f4339b7b887e54543c23f17f14c1c1cc1cbc7ac7
michaldobiezynski <michaldobiezynski@hotmail.com>
2022-12-18 12:35:54 +0000

    capabilities of Three.js isometric perspective
    capabilities of Three.js isometric perspective

--- webgl.js
+++ webgl.js
@@ -5,6 +5,8 @@ global.THREE = require("three");
 require("three/examples/js/controls/OrbitControls");
 
 const canvasSketch = require("canvas-sketch");
+const random = require("canvas-sketch-util/random");
+const palettes = require("nice-color-palettes");
 
 const settings = {
   // Make the loop animated
@@ -22,31 +24,52 @@ const sketch = ({ context }) => {
   });
 
   // WebGL background color
-  renderer.setClearColor("#000", 1);
+  renderer.setClearColor("hsl(0, 0%, 95%)", 1);
 
   // Setup a camera
-  const camera = new THREE.PerspectiveCamera(45, 1, 0.01, 100);
-  camera.position.set(2, 2, 4);
-  camera.lookAt(new THREE.Vector3());
+  const camera = new THREE.OrthographicCamera();
 
   // Setup camera controller
-  const controls = new THREE.OrbitControls(camera, context.canvas);
 
   // Setup your scene
   const scene = new THREE.Scene();
 
   // Setup a geometry
-  const geometry = new THREE.SphereGeometry(1, 32, 16);
+  const box = new THREE.BoxGeometry(0.5, 0.5, 0.5);
 
   // Setup a material
   const material = new THREE.MeshBasicMaterial({
     color: "red",
-    wireframe: true,
   });
 
+  const palette = random.pick(palettes);
+
   // Setup a mesh with geometry + material
-  const mesh = new THREE.Mesh(geometry, material);
-  scene.add(mesh);
+  for (let x = 0; x < 10; x++) {
+    for (let y = 0; y < 10; y++) {
+      const mesh = new THREE.Mesh(
+        box,
+        new THREE.MeshBasicMaterial({
+          color: random.pick(palette),
+        })
+      );
+      mesh.scale.set(
+        random.range(-1, 1),
+        random.range(-1, 1),
+        random.range(-1, 1)
+      );
+      mesh.position.set(
+        random.range(-1, 1),
+        random.range(-1, 1),
+        random.range(-1, 1)
+      );
+      mesh.scale.multiplyScalar(0.5);
+      scene.add(mesh);
+    }
+  }
+
+  // const mesh = new THREE.Mesh(geometry, material);
+  // scene.add(mesh);
 
   // draw each frame
   return {
@@ -54,17 +77,34 @@ const sketch = ({ context }) => {
     resize({ pixelRatio, viewportWidth, viewportHeight }) {
       renderer.setPixelRatio(pixelRatio);
       renderer.setSize(viewportWidth, viewportHeight, false);
-      camera.aspect = viewportWidth / viewportHeight;
+      const aspect = viewportWidth / viewportHeight;
+
+      // Ortho zoom
+      const zoom = 2.5;
+
+      // Bounds
+      camera.left = -zoom * aspect;
+      camera.right = zoom * aspect;
+      camera.top = zoom;
+      camera.bottom = -zoom;
+
+      // Near/Far
+      camera.near = -100;
+      camera.far = 100;
+
+      // Set position & look at world center
+      camera.position.set(zoom, zoom, zoom);
+      camera.lookAt(new THREE.Vector3());
+
+      // Update the camera
       camera.updateProjectionMatrix();
     },
     // Update & render your scene here
     render({ time }) {
-      controls.update();
       renderer.render(scene, camera);
     },
     // Dispose of events & renderer for cleaner hot-reloading
     unload() {
-      controls.dispose();
       renderer.dispose();
     },
   };
