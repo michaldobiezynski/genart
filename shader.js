3cb14af83ce79fe0cf4d88ea449cbd18e38d3b46
michaldobiezynski <michaldobiezynski@hotmail.com>
2022-12-19 15:11:55 +0000

    introducing noise
    introducing noise

--- shader.js
+++ shader.js
@@ -17,22 +17,29 @@ const frag = glsl(`
   // uniform vec2 resolution;
   varying vec2 vUv;
 
+  #pragma glslify: noise = require('glsl-noise/simplex/3d');
+
   void main () {
-    vec3 colorA = vec3(1.0, 0.0, 0.0);
-    vec3 colorB = vec3(0.0, 0.0, 0.5);
+    // vec3 colorA = vec3(1.0, 0.0, 0.0);
+    // vec3 colorB = vec3(0.0, 0.0, 0.5);
 
     vec2 center = vUv - 0.5;
     center.x *= aspect;
-    float dist = length(center);
 
-    float alpha = smoothstep(0.251, 0.25, dist);
+
+    // float dist = length(center);
+
+    // float alpha = smoothstep(0.251, 0.25, dist);
     // float alpha = step(dist, 0.25);
 
 
 
-    vec3 color = mix(colorA, colorB, vUv.x);
+    // vec3 color = mix(colorA, colorB, vUv.x);
     // vec3 color = vec3(vUv.x);
-    gl_FragColor = vec4(color, alpha);
+
+    float n = noise(vec3(center * 2.0, time));
+
+    gl_FragColor = vec4(vec3(n), 1.0);
   }
 `);
 
